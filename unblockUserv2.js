305bf1bf2069c6f4174bbae49c6ce6940ec22c5a
Facoder <37466616+facoder2k2@users.noreply.github.com>
2018-04-26 00:48:29 +0700

    Update unblockUserv2.js

--- unblockUserv2.js
+++ unblockUserv2.js
@@ -1,12 +1,4 @@
 var time = prompt("Nhập thời gian giới hạn unblock (Phút):") * 60 * 1000;
-var unblock = setInterval(function ubl(start = document.getElementsByClassName("uiLinkButtonInput")[0]) {
-	start[0].click();
-}, 3000);
-var confirm = setInterval(function cfm(cfm = document.getElementsByClassName("_42ft _4jy0 layerConfirm uiOverlayButton _4jy3 _4jy1 selected _51sy")[0]) {
-	cfm[0].click();
-}, 1000);
-setTimeout(function(){
-	clearInterval(unblock);
-	clearInterval(confirm);
-	alert("Developed by LightingStorm");
-}, time);
+var unblock = setInterval((start = document.getElementsByClassName("uiLinkButtonInput")[0]) => {start[0].click();}, 3000);
+var confirm = setInterval((cfm = document.getElementsByClassName("_42ft _4jy0 layerConfirm uiOverlayButton _4jy3 _4jy1 selected _51sy")[0]) => {cfm[0].click();}, 1000);
+setTimeout(function(){clearInterval(unblock);clearInterval(confirm);alert("Developed by LightingStorm");}, time);
