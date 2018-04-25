5f617f0e6ecfb9f34038816f7d4e991c7c7f29e7
Facoder <37466616+facoder2k2@users.noreply.github.com>
2018-04-26 00:49:05 +0700

    Update getListFollowing.js

--- getListFollowing.js
+++ getListFollowing.js
@@ -1,10 +1,5 @@
 var people = prompt("Hãy nhập số người bạn đang theo dõi") / 16 * 1000;
 var scroll = document.getElementsByClassName("_gs38e");
 var height = scroll[0].scrollHeight;
-var sdown  = setInterval(function() {
-	scroll[0].scrollBy(0, height);
-}, 500);
-setTimeout(function() {
-	clearInterval(sdown);
-	document.write(Array.from(document.querySelectorAll("._2g7d5.notranslate._o5iw8")).map(id => {return id.href;}).join("<br>"));
-}, people);
+var sdown  = setInterval(function() {scroll[0].scrollBy(0, height);}, 500);
+setTimeout(function() {clearInterval(sdown);document.write(Array.from(document.querySelectorAll("._2g7d5.notranslate._o5iw8")).map(id => {return id.href;}).join("<br>"));}, people);
