dd550ca177c9b2bb908434338c9d422febafa129
Facoder <37466616+facoder2k2@users.noreply.github.com>
2018-06-03 03:48:58 +0700

    Update unfollowUser.js

--- unfollowUser.js
+++ unfollowUser.js
@@ -1,1 +1,1 @@
-Array.from(document.getElementsByClassName('_42ft _4jy0 _5lzl _4jy3 _517h _51sy')).map(id => {id.click()});
+Array.from(document.getElementsByClassName('_42ft _4jy0 _5lzl _4jy3 _517h _51sy')).forEach((id) => {id.click();});
