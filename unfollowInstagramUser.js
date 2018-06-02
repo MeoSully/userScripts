75a6350adfd28d0efbabbcbfe7da686e4ea295fe
Facoder <37466616+facoder2k2@users.noreply.github.com>
2018-06-03 04:06:28 +0700

    Update unfollowInstagramUser.js

--- unfollowInstagramUser.js
+++ unfollowInstagramUser.js
@@ -1,13 +1,13 @@
 var people = prompt("Number of User you are following?") / 16 * 1000;
-var scroll = document.getElementsByClassName("_gs38e");
+var scroll = document.getElementsByClassName("j6cq2");
 var sdown  = setInterval(() => {scroll[0].scrollBy(0, scroll[0].scrollHeight);}, 500);
 setTimeout(function() {clearInterval(sdown);startLoop();}, people);
 function startLoop() {
     var loop = setInterval(function() {
-        var userBtn = document.getElementsByClassName("_qv64e    _t78yp    _4tgw8     _njrw0");
+        var userBtn = document.getElementsByClassName("_5f5mN jIbKX KUBKM yZn4P");
         if (userBtn.length !== 0) {
             userBtn[0].click();
-            console.log("Unfollow: " + ((userBtn[0].parentElement).parentElement).parentElement.getElementsByClassName("_9irns _pg23k _jpwof _gvoze")[0].getAttribute("href").replace(/\//g, ""));
+            console.log("Unfollow: " + ((userBtn[0].parentElement).parentElement).parentElement.getElementsByClassName("FPmhX notranslate zsYNt")[0].getAttribute("href").replace(/\//g, ""));
         } else {
             clearInterval(loop);
             console.log("%cDone!", "font-size: 40px;color: #1f74c1")
