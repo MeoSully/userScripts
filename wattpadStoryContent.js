ef09b5c57098912e72636ea89aa30d13169d4405
Facoder <37466616+facoder2k2@users.noreply.github.com>
2018-06-03 03:56:35 +0700

    Update wattpadStoryContent.js

--- wattpadStoryContent.js
+++ wattpadStoryContent.js
@@ -1,1 +1,1 @@
-copy(Array.from(document.querySelectorAll("pre > p")).map((element) => {return element.innerHTML.split("<span")[0];}).join("<br>"));
+copy(Array.from(document.querySelectorAll("pre > p")).map((element) => element.innerHTML.split("<span")[0]).join("<br>"));
