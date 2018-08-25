3e69f076fe86398c0311b63f855de81a45e356a5
Facoder <37466616+facoder2k2@users.noreply.github.com>
2018-08-25 09:26:51 +0700

    Update getFbLargestImg.js

--- getFbLargestImg.js
+++ getFbLargestImg.js
@@ -1,5 +1,6 @@
 // You have to run this script in DevTools of browser, this can't use if add to browser bookmark
 new Promise((resolve, reject) => {
+	// Get ID of Image, if ID not exists => Let User Fill form
 	let url = document.URL, imgId;
 	if (url.match(/photo.php/g) !== null) {
 		imgId = url.match(/(?<=fbid=)\d+/g)[0];
@@ -8,6 +9,7 @@ new Promise((resolve, reject) => {
 	}
 	resolve({id: imgId});
 }).then((data) => {
+	// Automatic get facebook token at your profile page
 	return new Promise((resolve, reject) => {
 		let xhr = new XMLHttpRequest();
 		xhr.open("GET", "https://www.facebook.com/me/", false);
@@ -19,6 +21,7 @@ new Promise((resolve, reject) => {
 		xhr.send(null);
 	});
 }).then((data) => {
+	// Call API => Open Largest Image in New tab
 	fetch(`https://graph.facebook.com/v3.1/${data.id}?fields=images&access_token=${data.token}`).then((res) => res.json()).then((res) => {
 		window.open(res.images[0].source, "_blank");
 	});
