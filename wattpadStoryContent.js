7eee4750c786b977574b57a75fbb6471245e6b2b
Facoder <37466616+facoder2k2@users.noreply.github.com>
2018-09-08 02:02:58 +0700

    Update wattpadStoryContent.js

--- wattpadStoryContent.js
+++ wattpadStoryContent.js
@@ -1,1 +1,20 @@
-copy(Array.from(document.querySelectorAll("pre > p")).map((element) => element.innerHTML.split("<span")[0]).join("<br>"));
+function scroll() {
+	return new Promise((resolve, reject) => {
+		let interval = setInterval(function() {
+			if (document.querySelector(".last-page > div") !== null) {
+				clearInterval(interval);
+				resolve(document.body.scrollHeight);
+            } else {
+				window.scrollBy(0, 2000);
+            }
+        }, 1000);
+    });
+}
+
+scroll().then((e) => {
+	console.log(`Document Height: ${e}`);
+	let textArea = document.createElement("textarea");
+	textArea.setAttribute("style", "width: 100%;height: -webkit-fill-available;");
+	textArea.value = Array.from(document.querySelectorAll("pre > p")).map((element) => element.innerHTML.split("<span")[0]).join("\n");
+	document.querySelector(".last-page > div").appendChild(textArea);
+});
