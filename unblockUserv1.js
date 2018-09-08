4dd85aae04a8b9610d36549057ffe91384b66af1
Facoder <37466616+facoder2k2@users.noreply.github.com>
2018-09-08 22:41:41 +0700

    Delete unblockUserv1.js

--- unblockUserv1.js
+++
@@ -1,3 +0,0 @@
-var unblock = setInterval(function ubl() {document.getElementsByClassName("uiLinkButtonInput")[0].click()}, 3000);
-var confirm = setInterval(function cfm() {document.getElementsByClassName("_42ft _4jy0 layerConfirm uiOverlayButton _4jy3 _4jy1 selected _51sy")[0].click();}, 1000);
-// Stop: clearInterval(unblock);clearInterval(confirm);
