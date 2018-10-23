a361198d6762921da8441f42346d793476958286
Facoder <37466616+facoder2k2@users.noreply.github.com>
2018-10-23 12:23:21 +0000

    Update getListFollowing.js

--- getListFollowing.js
+++ getListFollowing.js
@@ -1,5 +1,2 @@
-var people = prompt("Number of User you are following?") / 16 * 1000;
-var scroll = document.getElementsByClassName("j6cq2");
-var height = scroll[0].scrollHeight;
-var sdown  = setInterval(function() {scroll[0].scrollBy(0, height);}, 500);
+var people = prompt("Number of User you are following?") / 16 * 1000, scroll = document.getElementsByClassName("j6cq2"), height = scroll[0].scrollHeight, sdown = setInterval(function() {scroll[0].scrollBy(0, height);}, 500);
 setTimeout(function() {clearInterval(sdown);document.write(Array.from(document.querySelectorAll(".FPmhX.notranslate.zsYNt")).map((id) => id.href).join("<br>"));}, people);
