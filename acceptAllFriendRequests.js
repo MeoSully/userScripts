1e4dd69a6f8ed05ce3d431bc989f944537abeddf
Facoder <37466616+facoder2k2@users.noreply.github.com>
2018-10-23 12:34:20 +0000

    Update acceptAllFriendRequests.js

--- acceptAllFriendRequests.js
+++ acceptAllFriendRequests.js
@@ -2,7 +2,7 @@ Array.from(document.querySelectorAll(".friendRequestItem")).map((e) => e.getAttr
 	return cPromise.then((e) => {
 		let f = new FormData(), dtsg = require("DTSGInitialData").token || document.querySelector('[name="fb_dtsg"]').value, yourId = require("CurrentUserInitialData").USER_ID || document.cookie.match(/c_user=([0-9]+)/)[1], obj = {"action": "confirm", "id": id, "__user": yourId, "__a": 1, "fb_dtsg": dtsg};
 
-		Object.keys(obj).forEach((key) => {f.append(key, obj[key]);});;
+		Object.keys(obj).forEach((key) => {f.append(key, obj[key]);});
 
 		return new Promise((resolve, reject) => {
 			fetch("https://www.facebook.com/requests/friends/ajax/?dpr=1", {method: "POST", body: f, credentials: "include"}).then((e) => (String(e.status).match(/^2/g)) ? (console.log(`Accepted Friend Request From: ${id}`), resolve()) : (console.log("Something went wrong :("), reject()));
