da208132e30acee7fd088ade66d3c0af281c6d31
Facoder <37466616+facoder2k2@users.noreply.github.com>
2018-10-23 12:33:51 +0000

    Update acceptAllFriendRequests.js

--- acceptAllFriendRequests.js
+++ acceptAllFriendRequests.js
@@ -1,22 +1,11 @@
 Array.from(document.querySelectorAll(".friendRequestItem")).map((e) => e.getAttribute("data-id")).reduce((cPromise, id) => {
 	return cPromise.then((e) => {
-		let f = new FormData(),
-			dtsg = require("DTSGInitialData").token || document.querySelector('[name="fb_dtsg"]').value,
-			yourId = require("CurrentUserInitialData").USER_ID || document.cookie.match(/c_user=([0-9]+)/)[1];
+		let f = new FormData(), dtsg = require("DTSGInitialData").token || document.querySelector('[name="fb_dtsg"]').value, yourId = require("CurrentUserInitialData").USER_ID || document.cookie.match(/c_user=([0-9]+)/)[1], obj = {"action": "confirm", "id": id, "__user": yourId, "__a": 1, "fb_dtsg": dtsg};
 
-		f.append("action", "confirm");
-		f.append("id", id);
-		f.append("__user", yourId);
-		f.append("__a", 1);
-		f.append("fb_dtsg", dtsg);
+		Object.keys(obj).forEach((key) => {f.append(key, obj[key]);});;
 
 		return new Promise((resolve, reject) => {
-			fetch("https://www.facebook.com/requests/friends/ajax/?dpr=1", {method: "POST", body: f, credentials: "include"}).then((e) => {
-				if (String(e.status).match(/^2/g)) {
-					console.log(`Accepted Friend Request From: ${id}`);
-					resolve();
-				}
-			});
+			fetch("https://www.facebook.com/requests/friends/ajax/?dpr=1", {method: "POST", body: f, credentials: "include"}).then((e) => (String(e.status).match(/^2/g)) ? (console.log(`Accepted Friend Request From: ${id}`), resolve()) : (console.log("Something went wrong :("), reject()));
 		});
 	});
 }, Promise.resolve()).then((e) => {console.log("Done!");});
